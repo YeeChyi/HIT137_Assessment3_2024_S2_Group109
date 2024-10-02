bba23165b9eebf84638e6d799a4cecd89e3eb240
Macy <s382081@students.cdu.edu.au>
2024-10-03 09:15:59 +0930

    background loads

--- youtube.py
+++ youtube.py
@@ -8,15 +8,23 @@ screen_width = 500
 screen_height = 500
 
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption('Tank vs Zombie')
+pygame.display.set_caption('The Rise of the Zombies')
+
+#load images
+sun_img = pygame.image.load('night.jpg')
+sun_img = pygame.transform.scale(sun_img, (screen_width, screen_height))
 
 # to run game continuously
 run = True
 while run:
 
+    screen.blit(sun_img, (0,0))
+
 # to quit game
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    pygame.display.update()
 
 pygame.quit()
