b8b97eff707f934a9b14848317998a735346c1a2
Macy <s382081@students.cdu.edu.au>
2024-10-04 16:38:40 +0930

    animation

--- youtube2.py
+++ youtube2.py
@@ -2,17 +2,15 @@ import pygame
 
 pygame.init()
 
-
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('CLUB PENGUIN')
 
-# speed rate
+# framerate
 clock = pygame.time.Clock()
 FPS = 60
-
 
 # player moves
 moving_left = False
@@ -21,84 +19,94 @@ moving_right = False
 # to add colors
 BG = (144, 201, 120)
 def draw_bg():
-	screen.fill(BG)
-	
+    screen.fill(BG)
+    
 
 # creating a character
 class Penguin(pygame.sprite.Sprite):
-	def __init__(self, x, y, scale, speed):
-		pygame.sprite.Sprite.__init__(self)
-		self.speed = speed
-		self.direction = 1
-		self.flip = False
-		img = pygame.image.load('girl1.jpg')
-		self.image = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
-		self.rect = self.image.get_rect()
-		self.rect.center = (x, y)
+    def __init__(self, char_type, x, y, scale, speed):
+        pygame.sprite.Sprite.__init__(self)
+        self.char_type = char_type
+        self.speed = speed
+        self.direction = 1
+        self.flip = False
+
+        # for movement of player
+        self.animation_list = []
+        self.frame_index = 0
+        self.update_time = pygame.time.get_ticks()
+        for i in range(4):
+            img = pygame.image.load(f'img/{self.char_type}/idle/{i}.png')
+            img = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+            self.animation_list.append(img)
+        self.image = self.animation_list[self.frame_index]
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+
+    def move(self, moving_left, moving_right):
+        dx = 0
+        dy = 0
+
+        # assign movement if moving left or right
+        if moving_left:
+            dx = -self.speed
+            self.flip = True
+            self.direction = -1
+        if moving_right:
+            dx = self.speed
+            self.flip = False
+            self.direction = 1
+
+        self.rect.x += dx
+        self.rect.y += dy
+
+    def update_animation(self):
+        ANIMATION_COOLDOWN = 100
+        self.image = self.animation_list[self.frame_index]
+        if pygame.time.get_ticks() - self.update_time > ANIMATION_COOLDOWN:
+            self.update_time = pygame.time.get_ticks()
+            self.frame_index += 1
+            
+            if self.frame_index >= len(self.animation_list):
+                self.frame_index = 0
+
+    def draw(self):
+        screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
 
 
-# for movements
-def move(self, moving_left, moving_right):
-	dx = 0
-	dy = 0
-    
-
-if moving_left:
-			dx = -self.speed
-			self.flip = True
-			self.direction = -1
-if moving_right:
-			dx = self.speed
-			self.flip = False
-			self.direction = 1    
-
-self.rect.x += dx
-self.rect.y += dy
-
-
-def draw(self):
-		screen.blit(pygame.transform.flip(self.image, self.rect))
-
-
-
-player = Penguin(200, 200, 3, 5)
-
+player = Penguin('player', 200, 200, 3, 5)
 
 run = True
 while run:
 
-	clock.tick(FPS)
-	
-	draw_bg()
-	
-	player.draw()
-	
+    clock.tick(FPS)
 
-player.move(moving_left, moving_right)
+    draw_bg()
+    player.update_animation()
+    player.move(moving_left, moving_right)
+    player.draw()
 
+    for event in pygame.event.get():
+        # to quit game
+        if event.type == pygame.QUIT:
+            run = False
 
-for event in pygame.event.get():
-		#quit game
-		if event.type == pygame.QUIT:
-			run = False
+        # keyboard functions when pressed
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                moving_left = True
+            if event.key == pygame.K_d:
+                moving_right = True
+            if event.key == pygame.K_ESCAPE:
+                run = False
+                
+        # keyboard functions when not pressed
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a:
+                moving_left = False
+            if event.key == pygame.K_d:
+                moving_right = False
 
-   # keyboard functions when pressed
-if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_a:
-				moving_left = True
-			if event.key == pygame.K_d:
-				moving_right = True
-			if event.key == pygame.K_ESCAPE:
-				run = False
-				
-    # keyboard functions when not pressed
-if event.type == pygame.KEYUP:
-			if event.key == pygame.K_a:
-				moving_left = False
-			if event.key == pygame.K_d:
-				moving_right = False
-
-
-pygame.display.update()
+    pygame.display.update()
 
 pygame.quit()
