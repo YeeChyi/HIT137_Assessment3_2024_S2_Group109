9e22f333b09778f6bfbbaef566f62921d1b6cced
Macy <s382081@students.cdu.edu.au>
2024-10-04 21:28:10 +0930

    creating bullets

--- youtube2.py
+++ youtube2.py
@@ -19,6 +19,11 @@ GRAVITY = 0.75
 # player moves
 moving_left = False
 moving_right = False 
+shoot = False
+
+# load bullest
+bullet_img = pygame.image.load('img/icon/ammo.jpg').convert_alpha()
+
 
 # to add colors
 BG = (144, 201, 120)
@@ -28,6 +33,11 @@ def draw_bg():
     screen.fill(BG)
     pygame.draw.line(screen, RED, (0, 300), (SCREEN_WIDTH, 300))
     
+
+# shooting bullets
+
+
+
 
 # creating a character
 class Penguin(pygame.sprite.Sprite):
@@ -48,7 +58,7 @@ class Penguin(pygame.sprite.Sprite):
         self.action = 0  # idle
         self.update_time = pygame.time.get_ticks()
 
-        # player movements
+        # player movements - UPDATE THE WALKING
         animation_types = ['idle', 'walking', 'jumping']
         for animation in animation_types:
             # reset temporary list of images
@@ -56,7 +66,7 @@ class Penguin(pygame.sprite.Sprite):
             # count number of files in folder
             num_of_frames = len(os.listdir(f'img/{self.char_type}/{animation}'))
             for i in range(num_of_frames):
-                img = pygame.image.load(f'img/{self.char_type}/{animation}/{i}.png')
+                img = pygame.image.load(f'img/{self.char_type}/{animation}/{i}.png').convert_alpha()
                 img = pygame.transform.scale(img, (70, 70))
                 temp_list.append(img)
             self.animation_list.append(temp_list)
@@ -129,6 +139,28 @@ class Penguin(pygame.sprite.Sprite):
     def draw(self):
         screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
 
+# creating bullets
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y, direction):
+        pygame.sprite.Sprite.__init__(self)
+        self.speed = 10 
+        self.image = bullet_img
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+        self.direction = direction
+        
+    def update(self):
+        self.rect.x += self.speed * self.direction
+        # check if bullet has gone off screen
+        if self.rect.right < 0 or self.rect.left > SCREEN_WIDTH:
+            self.kill()
+
+
+# sprite groups
+bullet_group = pygame.sprite.Group()
+
+# load bullet image
+bullet_img = pygame.image.load('ammo.jpg')
 
 player = Penguin('player', 200, 200, 3, 5)
 
@@ -140,8 +172,16 @@ while run:
     draw_bg()
     player.update_animation()
     
-    # to check player actions to walk
+    # update and draw groups
+    bullet_group.update()
+    bullet_group.draw(screen)
+
+    # to check player actions 
     if player.alive:
+        if shoot:
+            bullet = Bullet(player.rect.centerx + (0.6 * player.rect.size * player.direction), player.rect.centery, player.direction)
+            bullet_group.add(bullet)
+
         if player.in_air:
             player.update_action(2)  # jump
         elif moving_left or moving_right:
@@ -159,10 +199,12 @@ while run:
 
         # keyboard functions when pressed
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a:
+            if event.key == pygame.K_a:  # move left
                 moving_left = True
-            if event.key == pygame.K_d:
+            if event.key == pygame.K_d:  # move right
                 moving_right = True
+            if event.key == pygame.K_SPACE:  # to shoot
+                shoot = True
             if event.key == pygame.K_w and player.alive:  # jump 
                 player.jump = True    
             if event.key == pygame.K_ESCAPE:
@@ -174,6 +216,8 @@ while run:
                 moving_left = False
             if event.key == pygame.K_d:
                 moving_right = False
+            if event.key == pygame.K_SPACE:
+                shoot = False
 
     pygame.display.update()
 
