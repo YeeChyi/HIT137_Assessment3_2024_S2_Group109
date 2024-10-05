4fad4b7d4702edd3b6ab41128a05d804a23ef0ca
Macy <s382081@students.cdu.edu.au>
2024-10-05 23:07:51 +0930

    scrolling of background

--- environment.py
+++ environment.py
@@ -1,34 +1,86 @@
 import pygame
 pygame.init()
 
+clock = pygame.time.Clock()
+FPS = 60
+
 # game window
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 640
 LOWER_MARGIN = 100
 SIDE_MARGIN = 300
 
-screen = pygame.display.set_mode((SCREEN_WIDTH + SIDE_MARGIN, SCREEN_HEIGHT+LOWER_MARGIN))
+screen = pygame.display.set_mode((SCREEN_WIDTH + SIDE_MARGIN, SCREEN_HEIGHT + LOWER_MARGIN))
 pygame.display.set_caption('ENVIRONMENT')
 
+# define variables
+ROWS = 16
+MAX_COLS = 150
+TILE_SIZE = SCREEN_HEIGHT//ROWS
+
+scroll_left = False
+scroll_right = False
+scroll = 0
+scroll_speed = 1
+
 # load images 
-snow_img = pygame.image.load('img/background/clouds.png').convert_alpha
-mountain_img = pygame.image.load('img/background/MTN.png').convert_alpha
-sky_img = pygame.image.load('img/background/sky.png').convert_alpha
+mountain_img = pygame.image.load('img/background/MTN.png').convert_alpha()
+tree_img = pygame.image.load('img/background/bigtrees.png').convert_alpha()
+clouds_img = pygame.image.load('img/background/clouds.png').convert_alpha()
+sky_img = pygame.image.load('img/background/sky.png').convert_alpha()
+
+# define colors
+BLUE = (0, 0, 255)
+WHITE = (255, 255, 255)
+RED = (200, 25, 25)
 
 # drawing background
 def draw_bg():
-    screen.blit(sky_img, (0,0))
+    screen.fill(BLUE)
+    width = sky_img.get_width()
+    for x in range(4):
+        screen.blit(sky_img, ((x * width) - scroll * 0.5, 0))
+        screen.blit(mountain_img, ((x * width) - scroll * 0.6, SCREEN_HEIGHT - mountain_img.get_height() - 50))
+        screen.blit(tree_img, ((x * width) - scroll * 0.7, SCREEN_HEIGHT - tree_img.get_height() - 5))
+        screen.blit(clouds_img, ((x * width) - scroll * 0.8, SCREEN_HEIGHT - clouds_img.get_height() - 70))
+
+# for grid
+def draw_grid():
+    for c in range(MAX_COLS + 1):
+        pygame.draw.line(screen, WHITE, (c * TILE_SIZE, 0),(c*TILE_SIZE, SCREEN_HEIGHT))
+
 
 
 run = True
 while run:
-
+    clock.tick(FPS)
     draw_bg()
+
+    # scroll map
+    if scroll_left and scroll > 0:
+        scroll -= 5 * scroll_speed
+    if scroll_right:
+        scroll += 5 * scroll_speed
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        # keyboard press
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                scroll_left = True
+            if event.key == pygame.K_RIGHT:
+                scroll_right = True
+            if event.key == pygame.K_RSHIFT:
+                scroll_speed = 0.5
 
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                scroll_left = False
+            if event.key == pygame.K_RIGHT:
+                scroll_right = False
+            if event.key == pygame.K_RSHIFT:
+                scroll_speed = 1
 
     pygame.display.update()
 
