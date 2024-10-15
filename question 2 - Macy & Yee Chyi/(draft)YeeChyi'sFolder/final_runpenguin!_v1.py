30de9629ecd420cfdeb9265e3efac9aab823326e
Yee Chyi Too <s368710@students.cdu.edu.au>
2024-10-15 20:17:57 +0930

    i give up on this draft haha

--- question 2 - Macy & Yee Chyi/(draft)YeeChyi'sFolder/final_runpenguin!_v1.py
+++ question 2 - Macy & Yee Chyi/(draft)YeeChyi'sFolder/final_runpenguin!_v1.py
@@ -1,6 +1,7 @@
 import pygame
 import os
 import random
+import csv
 
 # initialise pygame
 pygame.init()
@@ -25,12 +26,22 @@ FPS = 60
 
 # game variables
 GRAVITY = 0.75
-TITLE_SIZE = 40
+ROWS = 16
+COLS = 150
+TILE_SIZE = SCREEN_HEIGHT//ROWS
+TILE_TYPES = 18
+level = 1
 
 # define player moves
 moving_left = False
 moving_right = False
 shoot = False
+
+img_list = []
+for x in range(TILE_TYPES):
+    img = pygame.image.load(f'img/tiles/{x}.png')
+    img = pygame.transform.scale(img, (TILE_SIZE, TILE_SIZE))
+    img_list.append(img)
 
 # load bullet image
 bullet_img = pygame.image.load("img/icon/bullet.png").convert_alpha()
@@ -47,8 +58,17 @@ item_boxes = {
 # font for displaying text
 font = pygame.font.SysFont(None, 36)
 
+# display info 
+def draw_text(text, font, text_col, x, y):
+    img = font.render(text, True, text_col)
+    screen.blit(img, (x, y))
+
+def draw_bg():
+    screen.fill(BACKGROUND)
+
 # creating our character
-class Penguin(pygame.sprite.Sprite):
+class Penguin(
+    pygame.sprite.Sprite):
     def __init__(self, char_type, x, y, scale, speed, ammo):
         pygame.sprite.Sprite.__init__(self)
         self.alive = True    # player is alive at start
@@ -90,3 +110,60 @@ class Penguin(pygame.sprite.Sprite):
         self.image = self.animation_list[self.action][self.frame_index]
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
+    
+    def update(self):
+        self.update_animation()
+        self.check_alive()
+        if self.shoot_cooldown > 0:
+            self.shoot_cooldown -= 1
+            
+    def move(self, moveing_left, moving_right):
+        dx = 0
+        dy = 0
+        
+        # to keep the player within the screen
+        if self.rect.left < 0:
+            self.rect.left = 0
+        elif self.rect.right > SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        elif self.rect.bottom >= SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
+            
+        # assign movement if moving left or right
+        if moving_left:
+            dx = -self.speed
+            self.flip = True
+            self.direction = -1
+        if moving_right:
+            dx = self.speed
+            self.flip = False
+            self.direction = 1
+        
+        # jump action
+        if self.jump and self.in_air == False:
+            self.vel_y = -11
+            self.jump = False
+            self.in_air = True
+        
+        # apply gravity
+        self.vel_y += GRAVITY
+        if self.vel_y > 10:
+            self.vel_y
+        dy += self.vel_y
+        
+        # check collision with floor
+        if self.rect.bottom + dy > 609:
+            dy = 609 - self.rect.bottom
+            self.in_air = False      # check if allowed to jump
+        
+        self.rect.x += dx
+        self.rect.y += dy
+        
+        def shoot(self):
+            if self.shoot_cooldown == 0 and self.ammo > 0:
+                self.shoot_cooldown = 20
+                bullet = bullet(self.rect.centerx + (0.75 * self.rect.width * self.direction), self.rect.centery, self.direction)
+                bullet_group.add(bullet)
+                self.ammo -= 1
